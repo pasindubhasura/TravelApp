87c9eb0c957cd15f512e56656ce5f93b565800ac
onlineipl18@gmail.com <onlineipl18@gmail.com>
2021-08-11 21:29:55 +0530

    make routes folder

---
+++ backend/routes/guides.js
@@ -0,0 +1,28 @@
+//importing dependencies
+const express = require('express');
+const guide = require('../models/guide');
+
+
+//invoke express 
+const router = express.Router();
+
+//Insert guide deatils 
+router.post('/guide/add',(req,res)=>{
+
+    let newGuide = new Guide(req.body);
+
+    newGuide.save((err) =>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        return res.status(200).json({
+            success:"Guide created Successfully!"
+        });
+
+        });
+});
+
+
+module.exports = router;

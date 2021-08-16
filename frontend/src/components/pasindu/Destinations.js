0b90ebcc4bd0e935e9fb0fe4850e97a72ff030c3
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-16 15:43:31 +0530

    created destination component

---
+++ frontend/src/components/pasindu/Destinations.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Styled from "styled-components";
+
+export default function Destinations() {
+  return (
+    <MainDiv>
+      <h1>Hello</h1>
+    </MainDiv>
+  );
+}
+const MainDiv = Styled.div`
+margin:0px auto;
+background-color:grey;
+`;

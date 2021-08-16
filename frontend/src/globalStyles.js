4e6f274f81f765b1c5611b1b21eadfb4cb6e3666
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-16 15:43:01 +0530

    create some global styles

---
+++ frontend/src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding: 0;
+    font-family: 'Karla','Times New Roman', Times, sans-serif,serif
+}
+`;
+export default GlobalStyle;

b056806434860ba7ec9e55934914d521e3707123
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 03:39:59 +0530

    first change AccomdationHome

---
+++ frontend/src/components/Accommodation/AccommodationHome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import './AccStyles.css';
+
+export default class AccommodationHome extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

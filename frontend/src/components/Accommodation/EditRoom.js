0cf8bd4835ccde160d2907154a8a135996de3f71
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 05:11:18 +0530

    add table data retrieve function

--- frontend/src/components/Accommodation/EditRoom.js
+++ frontend/src/components/Accommodation/EditRoom.js
@@ -79,7 +79,7 @@ export default class EditRoom extends Component {
 
     //display accommdation
     retrieveAccommodations() {
-        axios.get('http://localhost:8070/accommodation/').then(res => {
+        axios.get('http://localhost:5001/accommodation/').then(res => {
             if (res.data.success) {
                 this.setState({
                     accommodations: res.data.existingAccommodations
@@ -89,6 +89,66 @@ export default class EditRoom extends Component {
         });
     }    
     
+    onSubmit = (e) => {
+
+        e.preventDefault();
+        if (formValid(this.state)) {
+            const id = this.props.match.params.id;
+            const { accName, roomNo, noOfBeds, airCondition, price, description, availability } = this.state;
+            const data = {
+                accName: accName,
+                roomNo: roomNo,
+                noOfBeds: noOfBeds,
+                airCondition: airCondition,
+                price: price,
+                description: description,
+                availability: availability
+            }
+        //console.log(data)
+        axios.put(`http://localhost:5001/room/update/${id}`, data).then((res) => {
+            if (res.data.success) {
+                toast.success('Room no ' + this.state.roomNo + ' Update Successfully !');
+                this.setState(
+                    {
+                        accName: "",
+                        roomNo: Number,
+                        noOfBeds: Number,
+                        airCondition: "",
+                        price: Number,
+                        description: "",
+                        availability: ""
+                    }
+                )
+            } else {
+                toast.error("You have an Error in Updating");
+            }
+        });
+    }
+    else
+        alert("Please Enter Details Correclty !");
+    };    
+    
+    //calling the accommodation api
+    componentDidMount() {
+        //retrieve  Accommodations name
+        this.retrieveAccommodations();
+        const id = this.props.match.params.id;
+        axios.get(`http://localhost:5001/room/${id}`).then((res) => {
+            if (res.data.success) {
+                this.setState({
+                    accName: res.data.room.accName,
+                    roomNo: res.data.room.roomNo,
+                    noOfBeds: res.data.room.noOfBeds,
+                    airCondition: res.data.room.airCondition,
+                    price: res.data.room.price,
+                    description: res.data.room.description,
+                    availability: res.data.room.availability
+                });
+                console.log(this.state.room);
+            }
+        });
+    }   
+
     render() {
         return (
             <div>

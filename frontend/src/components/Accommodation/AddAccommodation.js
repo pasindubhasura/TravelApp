3836a005daad74e9e4b72053aafb4d51e787d3f7
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 04:10:50 +0530

    add form content to addaccommodation

--- frontend/src/components/Accommodation/AddAccommodation.js
+++ frontend/src/components/Accommodation/AddAccommodation.js
@@ -97,8 +97,81 @@ export default class AddAccommodation extends Component {
 
     render() {
         return (
-            <div>
-                
+            <div className="container containerTop">
+                <div className="row">
+                    <div className="col-1" />
+                    <div className="col-10">
+                        <div className="row">
+                            <div className="col position-relative link">
+                                <p><a href="/Accommodation_Home/">Accommodation Management</a> {'>'} <a href="/Accommodation_Home/Accommodation/">Accommodation</a> {'>'} Add Accommodation</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-9 position-relative">
+                                <h2>Add Accommodation</h2>
+                                <ToastContainer />
+                            </div>
+                            <hr className="hr" style={{ height: '2px', color: '#0a90e8' }} />
+                        </div>
+                        <div className="shadowBoxForm">
+                            <div className="row">
+                                <div className="col-6 form">
+                                    <form onSubmit={this.onSubmit}>
+                                        <div className="form-row">
+                                            <div className="form-group col" style={{ marginTop: '15px' }}>
+                                                <label>Accommodation type : </label>
+                                                <select id="type" className="form-control" name="type" onChange={this.handleTypeChange} value={this.state.accommodationType} required>
+                                                    <option selected>Choose type...</option>
+                                                    <option value="Hotel">Hotel</option>
+                                                    <option value="Motels">Motels</option>
+                                                    <option value="Gest House">Gest House</option>
+                                                    <option value="Apartment">Apartment</option>
+                                                    <option value="Luxury Lodges">Luxury Lodges</option>
+                                                </select>
+                                            </div>
+                                            <div className="form-group col" style={{ marginTop: '15px' }}>
+                                                <label>Accommodation Name : </label>
+                                                <input type="text" className="form-control" name="name" placeholder="#River side" value={this.state.name} onChange={this.handleInputChange}  required />
+                                                {formErrors.name.length > 5 &&(
+                                                    <p style={{color:'red'}} className="errorMessage">{formErrors.name}</p>
+                                                )}                                                
+                                            </div>                                         
+                                            <div className="form-group col" style={{ marginTop: '15px' }}>
+                                                <label>Number of Rooms : </label>
+                                                <input type="number" className="form-control" name="noOfRomm" placeholder="#2" value={this.state.noOfRomm} onChange={this.handleInputChange}  required />
+                                                {formErrors.noOfRomm < 1   ||(
+											        <p style={{color:'red'}} className="errorMessage">{formErrors.noOfRomm}</p>
+										        )}                                                                                                                                                 
+                                            </div>                                                                                  
+                                            <div className="form-group col" style={{ marginTop: '15px' }}>
+                                                <label>Telephone Number : </label>
+                                                <input type="number" className="form-control" id="mobile" name="mobile" placeholder="011456XXXX" value={this.state.mobile} onChange={this.handleInputChange}  required />
+                                                {formErrors.mobile.length > 10  &&(
+                                                    <p style={{color:'red'}} className="errorMessage">{formErrors.mobile}</p>
+                                                )}
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="form-group btndriver col-12" style={{ marginTop: '15px' }}>
+                                                <div className="form-group col" style={{ marginTop: '15px' }}>
+                                                    <button type="submit" className="btn btn-success acc-button" ><i className="far fa-save"></i>&nbsp;Save</button>&nbsp;&nbsp;
+                                                    <button type="reset" className="btn btn-primary acc-button"><i class="fas fa-eraser"></i>&nbsp;Clear</button>
+                                                </div>
+                                                <div className="form-group col" style={{ marginTop: '15px' }}>
+                                                    <a href="/Accommodation_Home/Accommodation/" className="btn btn-danger acc-button"><i className="fas fa-times"></i>&nbsp;Cancel</a>
+                                                </div>
+                                            </div>
+                                            <div className="col-6" />
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="col-6 accImage">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-1" />
+                </div>
             </div>
         )
     }

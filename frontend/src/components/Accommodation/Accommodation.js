0dcf25fa668e02664bbad2b833b684075394a07f
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 03:58:52 +0530

    add display view in accommodation

--- frontend/src/components/Accommodation/Accommodation.js
+++ frontend/src/components/Accommodation/Accommodation.js
@@ -78,8 +78,75 @@ export default class Accommodation extends Component {
 
     render() {
         return (
-            <div>
-                
+            <div className="container containerTop">
+                <div className="row">
+                    <div className="col-1"/>
+                    <div className="col-11">
+                        <div className="row">
+                            <div className="col position-relative link">
+                                <p><a href="/Accommodation_Home/">Accommodation Management</a> {'>'} Accommodations</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-9 position-relative">
+                                <h2>Accommodations</h2>
+                                <ToastContainer/>                                
+                            </div>
+                            <hr className="hr" style={{ height: '2px', color: '#0a90e8' }} />
+                        </div>
+                        <div className="row">
+                            <div className="col-3 buttons">
+                                <a href="/Accommodation_Home/Accommodation/add" type="button" class="button" ><span><i class="fal fa-plus-circle"></i>&nbsp;&nbsp;Add Accommodation</span></a><br /><br />
+                            </div>
+                            <div className="col-3 buttons">
+                                <a href="#" type="button" class="button" ><span><i class="fas fa-download"></i>&nbsp;&nbsp;Download Report</span></a><br /><br />
+                            </div>
+                            <div className="col-2">                                                                
+                            </div>
+                            <div className="col-4 search position-relative">
+                                <ReactTooltip />
+                                <i className="fa fa-search"></i> <input className="form-control" type="Search" placeholder="Search an accommodation" name="searchQuery" data-tip="Enter accommodation type or name" data-type="dark" onChange={this.handleSearchArea} />
+                            </div>
+                        </div>
+                        <div className="shadowBox">
+                            <div className="row">
+                                <div className="col-12 ">
+                                    <table class="table table-hover">
+                                        <thead className="table-secondary">
+                                            <tr>
+                                                <th scope="col">No</th>
+                                                <th scope="col">Accommodation Type</th>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">No of Room</th>
+                                                <th scope="col">Phone number</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        {this.state.accommodations.map((accommodations, index) => (
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row"><a href={`/accommodation/${accommodations._id}`} style={{ textDecoration: 'none' }}>{index + 1}</a></th>
+                                                    <td>{accommodations.accommodationType}</td>
+                                                    <td>{accommodations.name}</td>
+                                                    <td>{accommodations.noOfRomm}</td>
+                                                    <td>{accommodations.mobile}</td>
+                                                    <td>
+                                                        <a href={`/Accommodation_Home/Accommodation/edit/${accommodations._id}`} type="button" class="btn btn-success" style={{width:'95px', margin:'2px'}}>
+                                                            <i class="far fa-edit"></i>&nbsp;Edit
+                                                        </a>&nbsp;&nbsp;
+                                                        <a href="#" type="button" class="btn btn-danger" onClick={() => this.onDelete(accommodations._id)}>
+                                                            <i className="far fa-trash-alt"></i>&nbsp;Delete
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        ))}
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

dadd50e40137386457f68e012d2d80fa804f005c
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 04:21:58 +0530

    fix some changes accomdations

---
+++ frontend/src/components/Accommodation/EditAccommodation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EditAccommodation extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

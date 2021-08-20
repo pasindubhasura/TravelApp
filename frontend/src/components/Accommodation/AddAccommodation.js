8a8dc036d077e10e52594e8af7a569dfc904e050
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 04:06:13 +0530

    create AddAccommodation.js file

---
+++ frontend/src/components/Accommodation/AddAccommodation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import './AccStyles.css';
+
+
+export default class AddAccommodation extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

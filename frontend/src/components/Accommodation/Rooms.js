b3b9867e59a8867f3a626e9fc905287f1108d834
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 04:44:12 +0530

    add changes in accommodation

--- frontend/src/components/Accommodation/Rooms.js
+++ frontend/src/components/Accommodation/Rooms.js
@@ -24,7 +24,7 @@ export default class Rooms extends Component {
     }
 
     retrieveRooms(){
-        axios.get(`http://localhost:8070/room/`).then(res => {
+        axios.get(`http://localhost:5001/room/`).then(res => {
             if(res.data.success){
                 this.setState({
                     rooms:res.data.existingRooms
@@ -32,7 +32,7 @@ export default class Rooms extends Component {
                 console.log(this.state.rooms);
             }
         });
-    }    
+    }   
 
 
     //delete function with confirmation
@@ -46,7 +46,7 @@ export default class Rooms extends Component {
           })
           .then((willDelete) => {
             if (willDelete) {
-                axios.delete(`http://localhost:8070/room/delete/${id}`).then((res) => {
+                axios.delete(`http://localhost:5001/room/delete/${id}`).then((res) => {
 
                     swl('Room successfully Deleted',{
                       icon: "success",
@@ -71,7 +71,7 @@ export default class Rooms extends Component {
     //search room function
     handleSearchArea=(e)=>{
         const searchKey=e.currentTarget.value.toLowerCase();
-        axios.get('http://localhost:8070/room/').then(res => {
+        axios.get('http://localhost:5001/room/').then(res => {
             if(res.data.success){
                 this.filterData(res.data.existingRooms,searchKey);
             }
@@ -80,8 +80,81 @@ export default class Rooms extends Component {
 
     render() {
         return (
-            <div>
-                
+            <div className="container containerTop">
+                <div className="row">
+                    <div className="col-1"/>
+                    <div className="col-11">
+                        <div className="row">
+                            <div className="col position-relative link">
+                                <p><a href="/Accommodation_Home/">Accommodation Management</a> {'>'} Rooms</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-9 position-relative">
+                                <h2>Accommodation &nbsp;-&nbsp;Rooms</h2>                              
+                                <ToastContainer/>                                
+                            </div>
+                            <hr className="hr" style={{ height: '2px', color: '#0a90e8' }} />
+                        </div>
+                        <div className="row">
+                            <div className="col-2 buttons">
+                                <a href="/Accommodation_Home/Rooms/add" type="button" class="button" ><span><i class="fal fa-plus-circle"></i>&nbsp;&nbsp;Add Rooms</span></a><br /><br />
+                            </div>
+                            <div className="col-4 buttons">
+                                <a href="#" type="button" class="button" ><span><i class="fas fa-download"></i>&nbsp;&nbsp;Download Report</span></a><br /><br />
+                            </div>
+                            <div className="col-2">                                                                
+                            </div>
+                            <div className="col-4 search position-relative">
+                                <ReactTooltip />
+                                <i className="fa fa-search"></i> <input className="form-control" type="Search" placeholder="Search an accommodation" name="searchQuery" data-tip="Enter accommodation name OR room no OR availability" data-type="dark" onChange={this.handleSearchArea} />
+                            </div>
+                        </div>
+                        <div className="shadowBox">
+                            <div className="row">
+                                <div className="col-12 ">
+                                    <table class="table table-hover">
+                                        <thead className="table-secondary">
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Accommodation Name</th>
+                                                <th scope="col">Room No</th>
+                                                <th scope="col">No of Beds</th>
+                                                <th scope="col">Air Condition</th>
+                                                <th scope="col">price</th>
+                                                <th scope="col">Description</th>
+                                                <th scope="col">Availability</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        {this.state.rooms.map((rooms, index) => (
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row"><a href={`/accommodation/${rooms._id}`} style={{ textDecoration: 'none' }}>{index + 1}</a></th>
+                                                    <td>{rooms.accName}</td>
+                                                    <td>{rooms.roomNo}</td>
+                                                    <td>{rooms.noOfBeds}</td>
+                                                    <td>{rooms.airCondition}</td>
+                                                    <td>{rooms.price}</td>
+                                                    <td>{rooms.description}</td>
+                                                    <td>{rooms.availability}</td>
+                                                    <td>
+                                                        <a href={`/Accommodation_Home/Rooms/edit/${rooms._id}`} type="button" class="btn btn-success" style={{width:'95px', margin:'2px'}}>
+                                                            <i class="far fa-edit"></i>&nbsp;Edit
+                                                        </a>&nbsp;&nbsp;
+                                                        <a href="#" type="button" class="btn btn-danger" onClick={() => this.onDelete(rooms._id)}>
+                                                            <i className="far fa-trash-alt"></i>&nbsp;Delete
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        ))}
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

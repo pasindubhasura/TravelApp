f32439e390ec523cf7700b44cd110340f04b1213
Charith <wcharithbuddhipriya@gmail.com>
2021-08-21 03:56:35 +0530

    add retreive part Accomdation

--- frontend/src/components/Accommodation/Accommodation.js
+++ frontend/src/components/Accommodation/Accommodation.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react'
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import ReactTooltip from 'react-tooltip';
+import swl from 'sweetalert'
+import './AccStyles.css';
+
 
 export default class Accommodation extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={
+            accommodations:[],
+            name:""
+        };
+    }
+
+
+    //calling the accommodation api
+    componentDidMount(){
+        this.retrieveAccommodations();
+    }
+    
+    //display accommdation
+    retrieveAccommodations(){
+        axios.get('http://localhost:8070/accommodation/').then(res => {
+            if(res.data.success){
+                this.setState({
+                    accommodations:res.data.existingAccommodations
+                });
+                console.log(this.state.accommodations)
+            }
+        });
+    }    
+
+
+    
     render() {
         return (
             <div>

d1ca720652d20af26cc7524eb7211de3e11c5e2b
Charith <wcharithbuddhipriya@gmail.com>
2021-09-18 07:56:21 +0530

    change home.css to Dashboard.css

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import Calendar from 'react-calendar'
-import './home.css';
+import './Dashboard.css';
 import "./calendar.css";
 
 
